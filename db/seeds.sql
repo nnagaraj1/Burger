23c022a336fc6b82da22c1def0e16f5e85d90c7d
Neeni Nagaraj <neeninagaraj@neenis-air.attlocal.net>
2018-06-05 16:58:14 -0400

    updated eat-da-burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Avocado Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipotle Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern-Fried Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Veggie Burger', true);
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var route = require("./controllers/burgers_controller");
+
+app.use("/", route);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+})
